fe9d1c850b701c3fe4d966c4871786ae10f1f5ac
caamomilaa <163582100+caamomilaa@users.noreply.github.com>
2025-05-22 10:56:05 +0200

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,68 +1,69 @@
 import { useForm } from 'react-hook-form';
 import { FORM_VALIDATIONS } from '../../constants/validations';
+import { ErrorMessage } from './contact.styles';
 
 const Contact = () => {
-	const {
-		handleSubmit,
-		register,
-		formState: { errors }
-	} = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm({ mode: 'onBlur' }); //   Para validar una vez salgas del input
 
-	return (
-		<>
-			<form onSubmit={handleSubmit()}>
-				<h1>Contact Us</h1>
+  return (
+    <>
+      <form onSubmit={handleSubmit()}>
+        <h1>Contact Us</h1>
 
-				<div>
-					<label htmlFor='name'>First Name*</label>
-					<input type='text' {...register('name', FORM_VALIDATIONS.NAME)} />
-					{errors.name && <span>This field is required</span>}
-				</div>
-				<div>
-					<label htmlFor='surname'>Last Name*</label>
-					<input type='text' {...register('surname', FORM_VALIDATIONS.NAME)} />
-					{errors.surname && <span>This field is required</span>}
-				</div>
-				<div>
-					<p>Email Adress*</p>
-					<input type='text' {...register('email', FORM_VALIDATIONS.EMAIL)} />
-					{errors.email && <span>Please enter a valid email address</span>}
-				</div>
-				<div>
-					<p>Query Type*</p>
-					<input
-						type='checkbox'
-						{...register('general', FORM_VALIDATIONS.QUERY)}
-					/>
-					<label htmlFor='general'>General Enquiry</label>
+        <div>
+          <label htmlFor='name'>First Name*</label>
+          <input type='text' {...register('name', FORM_VALIDATIONS.NAME)} />
+          {<ErrorMessage>{errors?.name?.message}</ErrorMessage>}
+        </div>
+        <div>
+          <label htmlFor='surname'>Last Name*</label>
+          <input type='text' {...register('surname', FORM_VALIDATIONS.NAME)} />
+          {<ErrorMessage>{errors?.surname?.message}</ErrorMessage>}
+        </div>
+        <div>
+          <p>Email Adress*</p>
+          <input type='text' {...register('email', FORM_VALIDATIONS.EMAIL)} />
+          {<ErrorMessage>{errors?.email?.message}</ErrorMessage>}
+        </div>
+        <div>
+          <p>Query Type*</p>
+          <input
+            type='checkbox'
+            {...register('general', FORM_VALIDATIONS.QUERY)}
+          />
+          <label htmlFor='general'>General Enquiry</label>
 
-					<input
-						type='checkbox'
-						{...register('support', FORM_VALIDATIONS.QUERY)}
-					/>
-					<label htmlFor='support'>Support Request</label>
-				</div>
-				<div>
-					<p>Message*</p>
-					<input
-						type='text'
-						{...register('message', FORM_VALIDATIONS.MESSAGE)}
-					/>
-				</div>
-				<div>
-					<input
-						type='checkbox'
-						{...register('consent', FORM_VALIDATIONS.CONSENT)}
-					/>
-					<label htmlFor='consent'>
-						I consent to being contacted by the team *
-					</label>
-					{errors.consent && <span>This field is required</span>}
-				</div>
-				<input type='submit' value='Submit' />
-			</form>
-		</>
-	);
+          <input
+            type='checkbox'
+            {...register('support', FORM_VALIDATIONS.QUERY)}
+          />
+          <label htmlFor='support'>Support Request</label>
+        </div>
+        <div>
+          <p>Message*</p>
+          <input
+            type='text'
+            {...register('message', FORM_VALIDATIONS.MESSAGE)}
+          />
+        </div>
+        <div>
+          <input
+            type='checkbox'
+            {...register('consent', FORM_VALIDATIONS.CONSENT)}
+          />
+          <label htmlFor='consent'>
+            I consent to being contacted by the team *
+          </label>
+          {<ErrorMessage>{errors?.consent?.message}</ErrorMessage>}
+        </div>
+        <input type='submit' value='Submit' />
+      </form>
+    </>
+  );
 };
 
 export default Contact;
--- src/constants/validations.js
+++ src/constants/validations.js
@@ -1,60 +1,61 @@
 const messages = {
-	name: {
-		required: 'This field is required',
-		wrong: 'Invalid name'
-	},
-	email: {
-		required: 'This field is required',
-		wrong: 'Please enter a valid email address'
-	},
-	query: {
-		selection: 'Please select a query type'
-	},
-	message: {
-		required: 'This field is required'
-	},
-	consent: {
-		selection: 'To submit this form, please consent to being contacted'
-	}
+  name: {
+    required: 'This field is required',
+    wrong: 'Invalid name'
+  },
+  email: {
+    required: 'This field is required',
+    wrong: 'Please enter a valid email address'
+  },
+  query: {
+    selection: 'Please select a query type'
+  },
+  message: {
+    required: 'This field is required'
+  },
+  consent: {
+    selection: 'To submit this form, please consent to being contacted'
+  }
 };
 
 const patterns = {
-	onlyLetters: /^[A-Za-z]+$/,
-	email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  onlyLetters: /^[A-Za-z]+$/,
+  email:
+    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
 };
 
 const nameValidation = {
-	required: messages.name.required,
-	pattern: {
-		value: patterns.onlyLetters,
-		message: messages.name.wrong
-	}
+  required: messages.name.required,
+  pattern: {
+    value: patterns.onlyLetters,
+    message: messages.name.wrong
+  }
 };
 const emailValidation = {
-	required: messages.email.required,
-	pattern: {
-		value: patterns.email,
-		message: messages.email.wrong
-	}
+  required: messages.email.required,
+  pattern: {
+    value: patterns.email,
+    message: messages.email.wrong
+  }
 };
 const queryValidation = {
-	required: messages.query.selection
-	//no se si vale la pena poner el texto arriba y esta linea aqui o mejor ponerlo todo junto
+  required: messages.query.selection
+  //no se si vale la pena poner el texto arriba y esta linea aqui o mejor ponerlo todo junto
 };
 const messageValidation = {
-	required: messages.message.required
+  required: messages.message.required
 };
 const consentValidation = {
-	required: messages.consent.selection
+  required: messages.consent.selection
 };
 
 export const FORM_VALIDATIONS = {
-	NAME: nameValidation,
-	EMAIL: emailValidation,
-	QUERY: queryValidation,
-	MESSAGE: messageValidation,
-	CONSENT: consentValidation
-	//por qué name en mayus?
+  NAME: nameValidation,
+  EMAIL: emailValidation,
+  QUERY: queryValidation,
+  MESSAGE: messageValidation,
+  CONSENT: consentValidation
+  //por qué name en mayus?
 };
 
 //VALIDACIONES
